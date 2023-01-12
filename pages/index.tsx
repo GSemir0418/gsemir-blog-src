6dbb8ec159fec460659a8b1545a79893ba08d46a
gsemir0418 <845217811@qq.com>
2023-01-12 07:54:52 +0000

    Head组件与Script组件实践

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
+import Head from "next/head"
 import Link from "next/link"
+import Script from "next/script"
 import { Profile } from "../components/Profile"
 
 export default function Home() {
   return (
     <>
-      <Profile/>
+      <Head>
+        <title>GSemir Blog</title>
+      </Head>
+      {/* 加载第三方js */}
+      <Script
+        src="https://cdn.bootcdn.net/ajax/libs/axios/1.2.2/axios.min.js"
+        // 浏览器闲置时加载
+        strategy="lazyOnload"
+        onLoad={() => {
+          console.log('axios is now loaded')
+        }}
+      />
+      <Profile />
       <p><Link href={'/post/1'}>Post 1</Link></p>
     </>
   )
--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function Article() {
   return (
     <>
+      <Head>
+        <title>Post 1</title>
+      </Head>
       <div>Post 1</div>
       <Link href="/">Back to home</Link>
     </>

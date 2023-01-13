4503f3132c8c34b29fb6ad3b81ec5a02ec6a2e16
gsemir0418 <845217811@qq.com>
2023-01-13 06:22:47 +0000

    使用动态路由静态生成博客页面

--- lib/blogs.ts
+++ lib/blogs.ts
@@ -2,9 +2,11 @@ import path from "path";
 import fs from "fs";
 import matter from "gray-matter";
 
-export type BlogDataType = { [key:string]: any }
+export type BlogDataType = { [key: string]: any };
 
 const blogsDirectory = path.join(process.cwd(), "blogs");
+
+// 按时间顺序获取全部博客meta数据
 export function getSortedBlogsData() {
   // 获取全部文件名
   const fileNames = fs.readdirSync(blogsDirectory);
@@ -19,7 +21,7 @@ export function getSortedBlogsData() {
 
     // 使用 gray-matter 解析 metadata 部分
     const matterResult = matter(fileContents);
-
+    console.log("111", matterResult);
     // 整合数据与文件名
     return {
       id,
@@ -34,4 +36,23 @@ export function getSortedBlogsData() {
       return -1;
     }
   });
+}
+
+// 获取全部blog名称（作为getStaticPaths返回值）
+export function getAllBlogIds() {
+  const fileNames = fs.readdirSync(blogsDirectory);
+  return fileNames.map((item) => {
+    return { params: { id: item.replace(/\.md$/, "") } };
+  });
+}
+
+// 根据id获取blogs meta数据
+export function getBlogDataById(id: string) {
+  const fullPath = path.join(blogsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+  const matterResult = matter(fileContents);
+  return {
+    id,
+    ...matterResult.data,
+  };
 }
--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,14 +1,44 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { Layout } from "../../components/Layout";
+import { BlogDataType, getAllBlogIds, getBlogDataById } from "../../lib/blogs";
 
-export default function Article() {
+const Article: NextPage<{ blogData: BlogDataType }> = ({ blogData }) => {
   return (
-    <>
+    <Layout>
       <Head>
-        <title>Post 1</title>
+        <title>{blogData.title}</title>
       </Head>
-      <div>Post 1</div>
+      {blogData.title}
+      <br />
+      {blogData.id}
+      <br />
+      {blogData.date}
       <Link href="/">Back to home</Link>
-    </>
+    </Layout>
   )
+}
+
+export default Article
+
+// 动态路由中必须要包含两个函数：getStaticProps getStaticPaths
+// getStaticPaths 返回一个id可能值的数组，用于动态生成不同的html
+// getStaticProps 根据id获取页面数据
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const ids = getAllBlogIds()
+  return {
+    paths: ids,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const blogData = getBlogDataById(params?.id as string);
+  return {
+    props: {
+      blogData,
+    },
+  };
 }
